5a790f6297ebe0f280bd9e0e546b62e8a692149b
gouthr <gouthr@gmail.com>
2016-09-05 09:57:27 -0700

    Longest Palindromic subsequence of a given string

--- src/strings/StringImpl.java
+++ src/strings/StringImpl.java
@@ -4,9 +4,36 @@ public class StringImpl {
 
 	public static void main(String[] args) {
 		StringImpl strImpl = new StringImpl();
+		
+		// Permutations of a given string
 		String str = "ABC";
 		StringBuilder sb = new StringBuilder(str);
 		strImpl.permute(sb, 0, sb.length()-1);
+		
+		// length of the longest palindromic subsequence
+		str = "geeks4geeks";
+		System.out.println("Length of the longest palindromic subsequence of the string: " + str + " is:" + strImpl.lps(str, 0, str.length()-1));		
+		
+	}
+	
+	/*
+	 * Length of the longest palindromic subsequence in a given string.
+	 * 
+	 * Given a sequence, find the length of the longest palindromic subsequence in it. 
+	 * For example, if the given sequence is “BBABCBCAB”, 
+	 * then the output should be 7 as “BABCBAB” is the longest palindromic subseuqnce in it.
+	 * “BBBBB” and “BBCBB” are also palindromic subsequences of the given sequence, but not the longest ones.
+	 */
+	public int lps(String str, int st, int end) {
+		if (str.charAt(st) == str.charAt(end) && st == end) {
+			return 1;
+		} else if (str.charAt(st) == str.charAt(end) && st+1 == end) {
+			return 2;
+		} else if (str.charAt(st) == str.charAt(end)) {
+			return 2 + lps(str, st+1, end-1);
+		} else {
+			return Math.max(lps(str, st+1, end), lps(str, st, end-1));
+		}
 	}
 	
 	public void permute(StringBuilder str, int start, int end) {
