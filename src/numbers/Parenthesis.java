7c38a9b95aaf1475253946417aceb1a6c5880559
gouthr <gouthr@gmail.com>
2016-09-11 18:30:29 -0700

    Print parenthesis valid combinations for a given count

--- src/numbers/Parenthesis.java
+++ src/numbers/Parenthesis.java
@@ -6,9 +6,16 @@ public class Parenthesis {
 
 	public static void main(String[] args) {
 		Parenthesis p = new Parenthesis();
+		
 		// Balanced parenthesis check
 		System.out.println("Balanced parenthesis check:");
 		System.out.println("Braces are balanced: " + p.isParenthesisBalanced("{()()((())}[]"));
+		
+		// Print all possible correct combinations of parenthesis given a count
+		int cnt = 3;
+		System.out.println("Possible combinations of braces for cnt: " + cnt + "  is :");
+		p.printParenthesis(cnt);
+		
 
 	}
 	
@@ -37,6 +44,28 @@ public class Parenthesis {
 		return false;
 	}
 	
+	public void printParenthesis(int cnt) {
+		if (cnt > 0) {
+			char[] str = new char[50];
+			_printParenthesis(str, 0, cnt, 0, 0);
+		}
+	}
+	
+	private void _printParenthesis(char[] paran, int index, int cnt, int open, int close) {
+		if (close == cnt) {
+			System.out.println(paran);
+			return;
+		}
+		if (open > close) {
+			paran[index] = '}';
+			_printParenthesis(paran, index+1, cnt, open, close+1);
+		} 
+		if (open < cnt) {
+			paran[index] = '{';
+			_printParenthesis(paran, index+1, cnt, open+1, close);
+		}
+	}
+	
 	private boolean isMatchingParan(char ele, char pop) {
 		if (ele == '}' && pop == '{') {
 			return true;
@@ -48,4 +77,6 @@ public class Parenthesis {
 			return false;
 		}
 	}
+	
+	private String parenthesis;
 }
