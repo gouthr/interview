9467c97f9fa726b9edc481c9a0dcdf867f79f645
gouthr <gouthr@gmail.com>
2016-09-17 00:54:22 -0700

    Solving sudoku given a valid grid of numbers in a 2D array

---
+++ src/numbers/Sudoku.java
@@ -0,0 +1,104 @@
+package numbers;
+
+public class Sudoku {
+	
+	public static void main(String[] args) {
+		Sudoku sod = new Sudoku();
+		int arr[][] = new int[][]{{3, 0, 6, 5, 0, 8, 4, 0, 0},
+								  {5, 2, 0, 0, 0, 0, 0, 0, 0},
+								  {0, 8, 7, 0, 0, 0, 0, 3, 1},
+								  {0, 0, 3, 0, 1, 0, 0, 8, 0},
+								  {9, 0, 0, 8, 6, 3, 0, 0, 5},
+								  {0, 5, 0, 0, 9, 0, 6, 0, 0},
+								  {1, 3, 0, 0, 0, 0, 2, 5, 0},
+								  {0, 0, 0, 0, 0, 0, 0, 7, 4},
+								  {0, 0, 5, 2, 0, 6, 3, 0, 0}};
+		if (sod.solveSudoku(arr)) {
+			for (int i=0; i<9; i++) {
+				for (int j=0; j<9; j++) {
+					System.out.print(arr[i][j] + " ");
+				}
+				System.out.println();
+			}
+		} else {
+			System.out.println("Sudoku solution does not exist.");
+		}
+	}
+	
+	public boolean solveSudoku(int[][] arr) {
+		Grid g = findUnassignedBox(arr) ;
+		if (g == null) {
+			return true;
+		}
+		for(int num=1; num<=9; num++) {
+			if (checkFitForBox(arr, g.row, g.col, num)) {
+				arr[g.row][g.col] = num;
+				if (solveSudoku(arr)) {
+					return true;
+				}
+				arr[g.row][g.col] = 0; // backtrack
+			}
+		}
+		return false;
+	}
+	
+	private Grid findUnassignedBox(int[][] arr) {
+		Grid g = null;
+		for (int i=0; i<9; i++) {
+			for (int j=0; j<9; j++) {
+				if (arr[i][j] == 0) {
+					return new Grid(i, j);
+				}
+			}
+		}
+		return g;
+	}
+	
+	private boolean checkFitForBox(int[][]arr, int row, int col, int num) {
+		if (!checkRowPresent(arr, row, num) && !checkColPresent(arr, col, num) && !checkBoxPresent(arr, row-(row%3), col-(col%3), num)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private boolean checkRowPresent(int[][] arr, int row, int num) {
+		for(int i=0; i<9; i++) {
+			if (arr[row][i] == num) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private boolean checkColPresent(int[][] arr, int col, int num) {
+		for(int i=0; i<9; i++) {
+			if (arr[i][col] == num) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private boolean checkBoxPresent(int[][] arr, int startRow, int startCol, int num) {
+		for(int i=0; i<3; i++) {
+			for (int j=0; j<3; j++) {
+				if (arr[startRow+i][startCol+j] == num) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	private class Grid {
+		public Grid(int row, int col) {
+			this.row = row;
+			this.col = col;
+		}
+		
+		private int row;
+		private int col;
+	}
+
+}
