f4c36f38b77bf8db01c1e6b0db9b99178650d5f8
gouthr <gouthr@gmail.com>
2018-03-31 11:26:49 -0700

    Binary sum

--- src/numbers/NumberImpl.java
+++ src/numbers/NumberImpl.java
@@ -604,6 +604,40 @@ public class NumberImpl {
 		return x;
 	}
 	
+	/**
+	 * Binary sum of 2 binary numbers provided as string input.
+	 * 
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public String binarySum(String a, String b) {
+		int i = a.length() - 1;
+		int j = b.length() - 1;
+		int sum = 0;
+		int carry = 0;
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(i>=0 || j>=0) {
+			sum = carry;
+			if (i>=0) {
+				sum += a.charAt(i) - '0';
+				i--;
+			}
+			if (j>=0) {
+				sum += b.charAt(j) - '0';
+				j--;
+			}
+			sb.append(sum%2);
+			carry = sum/2;
+		}
+		if (carry != 0) {
+			sb.append(carry);
+		}
+		return sb.reverse().toString();
+	}
+	
 	private class Interval {
 		int buy;
 		int sell;
