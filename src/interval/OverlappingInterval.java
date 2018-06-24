10636bb7434c93bd84d6c348058942620a4aed67
gouthr <gouthr@gmail.com>
2018-06-24 12:09:44 -0700

    no of meeting rooms

--- src/interval/OverlappingInterval.java
+++ src/interval/OverlappingInterval.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Stack;
 
 /**
@@ -56,6 +57,58 @@ public class OverlappingInterval {
 			Interval node = st.pop();
 			System.out.println("Interval start: " + node.start + " , interval end: " + node.end);
 		}
+	}
+	
+	/**
+	 * Given an array of meeting time intervals consisting of start and end
+	 * times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of
+	 * conference rooms required.
+	 * 
+	 * Example 1:
+	 * 
+	 * Input: [[0, 30],[5, 10],[15, 20]] Output: 2
+	 * 
+	 * Just want to share another idea that uses min heap, average time
+	 * complexity is O(nlogn).
+	 * 
+	 * @param intervals
+	 * @return
+	 */
+	public int minMeetingRooms(Interval[] intervals) {
+	    if (intervals == null || intervals.length == 0)
+	        return 0;
+	        
+	    // Sort the intervals by start time
+	    Arrays.sort(intervals, new Comparator<Interval>() {
+	        public int compare(Interval a, Interval b) { return a.start - b.start; }
+	    });
+	    
+	    // Use a min heap to track the minimum end time of merged intervals
+	    PriorityQueue<Interval> heap = new PriorityQueue<Interval>(intervals.length, new Comparator<Interval>() {
+	        public int compare(Interval a, Interval b) { return a.end - b.end; }
+	    });
+	    
+	    // start with the first meeting, put it to a meeting room
+	    heap.offer(intervals[0]);
+	    
+	    for (int i = 1; i < intervals.length; i++) {
+	        // get the meeting room that finishes earliest
+	        Interval interval = heap.poll();
+	        
+	        if (intervals[i].start >= interval.end) {
+	            // if the current meeting starts right after 
+	            // there's no need for a new room, merge the interval
+	            interval.end = intervals[i].end;
+	        } else {
+	            // otherwise, this meeting needs a new room
+	            heap.offer(intervals[i]);
+	        }
+	        
+	        // don't forget to put the meeting room back
+	        heap.offer(interval);
+	    }
+	    
+	    return heap.size();
 	}
 	
 	public static void main(String[] args) {
