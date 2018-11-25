6f0a39d3ac5075d442a32cf86439abea57b3fc04
gouthr <gouthr@gmail.com>
2018-11-25 11:27:34 -0800

    check for valid number

--- src/numbers/NumberImpl.java
+++ src/numbers/NumberImpl.java
@@ -1749,4 +1749,59 @@ public class NumberImpl {
 		str.setCharAt(i, str.charAt(j));
 		str.setCharAt(j, tmp);
 	}
+	
+	/**
+	 * Validate if a given string can be interpreted as a decimal number.
+
+		Some examples:
+		"0" => true
+		" 0.1 " => true
+		"abc" => false
+		"1 a" => false
+		"2e10" => true
+		" -90e3   " => true
+		" 1e" => false
+		"e3" => false
+		" 6e-1" => true
+		" 99e2.5 " => false
+		"53.5e93" => true
+		" --6 " => false
+		"-+3" => false
+		"95a54e53" => false
+	 * @param s
+	 * @return
+	 */
+	public boolean isValidNumber(String s) {
+		boolean pointSeen = false;
+		boolean eSeen = false;
+		boolean numSeen = false;
+		boolean numAfterE = false;
+		
+		s = s.trim();
+		for (int i=0; i<s.length(); i++) {
+			if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+				numSeen = true;
+				numAfterE = true;
+			}else if (s.charAt(i) == '.') {
+				if (pointSeen || eSeen) {
+					return false;
+				}
+				pointSeen = true;
+			} else if (s.charAt(i) == 'e') {
+				if (eSeen || !numSeen) {
+					return false;
+				}
+				eSeen = true;
+				numAfterE = false;
+			} else if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+				if (i != 0 && s.charAt(i-1) != 'e') {
+					return false;
+				}
+			} else {
+				return false;
+			}
+		}
+		
+		return numSeen && numAfterE;
+	}
 }
