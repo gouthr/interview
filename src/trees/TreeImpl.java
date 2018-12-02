cb68c0e97b8365e5f037eecd9e9c1c6f4413dafd
gouthr <gouthr@gmail.com>
2018-12-02 11:32:29 -0800

    house robber 3

--- src/trees/TreeImpl.java
+++ src/trees/TreeImpl.java
@@ -1169,6 +1169,35 @@ public class TreeImpl {
 		map.put(serialize, map.getOrDefault(serialize, 0)+1);
 		return serialize;
     }
+    
+	/**
+	 * The thief has found himself a new place for his thievery again. There is
+	 * only one entrance to this area, called the "root." Besides the root, each
+	 * house has one and only one parent house. After a tour, the smart thief
+	 * realized that "all houses in this place forms a binary tree". It will
+	 * automatically contact the police if two directly-linked houses were
+	 * broken into on the same night.
+	 * 
+	 * Determine the maximum amount of money the thief can rob tonight without
+	 * alerting the police.
+	 * 
+	 * @param root
+	 * @return
+	 */
+    public int rob(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int children = rob(root.left) + rob(root.right);
+        int sum = 0;
+        if (root.left != null) {
+            sum = rob(root.left.left) + rob(root.left.right);
+        }
+        if (root.right != null) {
+            sum += rob(root.right.left) + rob(root.right.right);
+        }
+        return Math.max(root.data + sum, children);
+    }
 	
 	/**
 	 * Tree node with parent pointer.
