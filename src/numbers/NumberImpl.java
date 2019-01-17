ae85619a5ce007517c0394888efe349e8ad7e2a9
gouthr <gouthr@gmail.com>
2019-01-16 17:17:17 -0800

    search in sorted array of unknown size

--- src/numbers/NumberImpl.java
+++ src/numbers/NumberImpl.java
@@ -1993,4 +1993,43 @@ public class NumberImpl {
         }
         return finalResult;
     }
+    
+    /**
+     * Search for target in a sorted array of unknown size.
+     * Given an integer array sorted in ascending order, write a function to search target in nums.  If target exists, then return its index, otherwise return -1. However, the array size is unknown to you. You may only access the array using an ArrayReader interface, where ArrayReader.get(k) returns the element of the array at index k (0-indexed).
+
+		You may assume all integers in the array are less than 10000, and if you access the array out of bounds, ArrayReader.get will return 2147483647.
+		
+		 
+		
+		Example 1:
+		
+		Input: array = [-1,0,3,5,9,12], target = 9
+		Output: 4
+		Explanation: 9 exists in nums and its index is 4
+     * @param reader
+     * @param target
+     * @return
+     */
+    /** - Below code is commented as ArrayReader interface in not implemented.
+    public int search(ArrayReader reader, int target) {
+        int i = 1;
+        while(reader.get(i) < target) {
+            i = i << 1;
+        }
+        int hi = i;
+        int lo = i >> 1;
+        while (lo <= hi) {
+            int mid = lo + (hi-lo)/2;
+            if (reader.get(mid) == target) {
+                return mid;
+            } else if (reader.get(mid) > target) {
+                hi = mid-1;
+            } else {
+                lo = mid+1;
+            }
+        }
+        return -1;
+    }
+    */
 }
