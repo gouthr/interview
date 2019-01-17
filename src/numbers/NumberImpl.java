7a66750f42f1f32fab1f637ddbf3ece7c6ec8234
gouthr <gouthr@gmail.com>
2019-01-16 17:01:59 -0800

    asteroid collision

--- src/numbers/NumberImpl.java
+++ src/numbers/NumberImpl.java
@@ -1934,4 +1934,63 @@ public class NumberImpl {
         }
         return Math.min(costs[n-1][0], Math.min(costs[n-1][1], costs[n-1][2]));
     }
+    
+    /**
+     * We are given an array asteroids of integers representing asteroids in a row.
+
+		For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
+		
+		Find out the state of the asteroids after all collisions. If two asteroids meet, the smaller one will explode. If both are the same size, both will explode. Two asteroids moving in the same direction will never meet.
+		
+		Example 1:
+		Input: 
+		asteroids = [5, 10, -5]
+		Output: [5, 10]
+		Explanation: 
+		The 10 and -5 collide resulting in 10.  The 5 and 10 never collide.
+		Example 2:
+		Input: 
+		asteroids = [8, -8]
+		Output: []
+		Explanation: 
+		The 8 and -8 collide exploding each other.
+		Example 3:
+		Input: 
+		asteroids = [10, 2, -5]
+		Output: [10]
+		Explanation: 
+		The 2 and -5 collide resulting in -5.  The 10 and -5 collide resulting in 10.
+		Example 4:
+		Input: 
+		asteroids = [-2, -1, 1, 2]
+		Output: [-2, -1, 1, 2]
+		Explanation: 
+		The -2 and -1 are moving left, while the 1 and 2 are moving right.
+		Asteroids moving the same direction never meet, so no asteroids will meet each other.
+     * @param asteroids
+     * @return
+     */
+    public int[] asteroidCollision(int[] asteroids) {
+    	int [] result = {};
+        if (asteroids == null || asteroids.length == 0) {
+            return result;
+        }
+        List<Integer> res = new ArrayList<>();
+        for (int i=0; i<asteroids.length; i++) {
+            if (asteroids[i] > 0 || res.isEmpty() || res.get(res.size()-1) < 0) {
+                res.add(asteroids[i]);
+            } else {
+            	if (-asteroids[i] >= res.get(res.size()-1)) {
+            		if (-asteroids[i] > res.remove(res.size()-1)) {
+            			i--;
+            		}
+            	}
+            }
+        }
+        int[] finalResult = new int[res.size()];
+        for (int i=0; i<res.size(); i++) {
+        	finalResult[i] = res.get(i);
+        }
+        return finalResult;
+    }
 }
