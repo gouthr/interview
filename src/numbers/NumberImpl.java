fd69f2d952918b9478d3e977fde28faaca021cd6
gouthr <gouthr@gmail.com>
2019-01-23 18:49:11 -0800

    3 sum

--- src/numbers/NumberImpl.java
+++ src/numbers/NumberImpl.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
@@ -2059,4 +2060,36 @@ public class NumberImpl {
         return -1;
     }
     */
+    
+    /**
+	 * 3sum Given an array nums of n integers, are there elements a, b, c in
+	 * nums such that a + b + c = 0? Find all unique triplets in the array which
+	 * gives the sum of zero.
+	 * 
+	 * Note:
+	 * 
+	 * The solution set must not contain duplicate triplets.
+	 * 
+	 * @param num
+	 * @return
+	 */
+    public List<List<Integer>> threeSum(int[] num) {
+        Arrays.sort(num);
+        List<List<Integer>> res = new LinkedList<>(); 
+        for (int i = 0; i < num.length-2; i++) {
+            if (i == 0 || (i > 0 && num[i] != num[i-1])) {
+                int lo = i+1, hi = num.length-1, sum = 0 - num[i];
+                while (lo < hi) {
+                    if (num[lo] + num[hi] == sum) {
+                        res.add(Arrays.asList(num[i], num[lo], num[hi]));
+                        while (lo < hi && num[lo] == num[lo+1]) lo++;
+                        while (lo < hi && num[hi] == num[hi-1]) hi--;
+                        lo++; hi--;
+                    } else if (num[lo] + num[hi] < sum) lo++;
+                    else hi--;
+               }
+            }
+        }
+        return res;
+    }
 }
