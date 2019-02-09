af30f2c3940565d0fecf9ddba1722d6baaf6cc61
gouthr <gouthr@gmail.com>
2019-02-09 12:48:24 -0800

    Longest common prefix string amongst an array of strings

--- src/strings/StringImpl.java
+++ src/strings/StringImpl.java
@@ -1626,4 +1626,35 @@ public class StringImpl {
     	}
     	return false;
     }
+    
+    /**
+     * Write a function to find the longest common prefix string amongst an array of strings.
+
+		If there is no common prefix, return an empty string "".
+		
+		Example 1:
+		
+		Input: ["flower","flow","flight"]
+		Output: "fl"
+		Example 2:
+		
+		Input: ["dog","racecar","car"]
+		Output: ""
+		Explanation: There is no common prefix among the input strings.
+     * @param strs
+     * @return
+     */
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) {
+            return "";
+        }
+        
+        String tmp = strs[0];
+        for (int i=1; i<strs.length; i++) {
+            while (strs[i].indexOf(tmp) != 0) { // String.indexOf of an empty string always returns 0
+                tmp = tmp.substring(0, tmp.length()-1);
+            }
+        }
+        return tmp;
+    }
 }
