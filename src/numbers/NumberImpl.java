87c35cd85e6edbadaf856c2279cd06a050a2a1f2
gouthr <gouthr@gmail.com>
2019-02-09 13:06:06 -0800

    happy numbers

--- src/numbers/NumberImpl.java
+++ src/numbers/NumberImpl.java
@@ -12,6 +12,7 @@ import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Random;
+import java.util.Set;
 import java.util.Stack;
 
 public class NumberImpl {
@@ -2203,6 +2204,40 @@ public class NumberImpl {
     			findWaysToTargetUtil(arr, n-1, target);
     }
     
+    /**
+     * Write an algorithm to determine if a number is "happy".
+
+		A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+		
+		Example: 
+		
+		Input: 19
+		Output: true
+		Explanation: 
+		1^2 + 9^2 = 82
+		8^2 + 2^2 = 68
+		6^2 + 8^2 = 100
+		1^2 + 0^2 + 0^2 = 1
+     * @param n
+     * @return
+     */
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (set.add(n)) {
+            int sum = 0;
+            while (n > 0) {
+                int rem = n%10;
+                sum+= (rem*rem);
+                n = n/10;
+            }
+            if (sum == 1) {
+                return true;
+            } else {
+                n = sum;
+            }
+        }
+        return false;
+    }
     
     public NumberImpl(){}
 }
