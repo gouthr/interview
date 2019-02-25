3687962996f9d4157e1057f24860112566681fdb
gouthr <gouthr@gmail.com>
2019-02-25 12:25:54 -0800

    Is subsequence optimized

--- src/strings/StringImpl.java
+++ src/strings/StringImpl.java
@@ -1652,6 +1652,42 @@ public class StringImpl {
     }
     
     /**
+     * Above Subsequence problem optimized.
+     * @param s
+     * @param t
+     * @return
+     */
+    // Follow-up: O(N) time for pre-processing, O(Mlog?) for each S.
+    // Eg-1. s="abc", t="bahbgdca"
+    // idx=[a={1,7}, b={0,3}, c={6}]
+    //  i=0 ('a'): prev=1
+    //  i=1 ('b'): prev=3
+    //  i=2 ('c'): prev=6 (return true)
+    // Eg-2. s="abc", t="bahgdcb"
+    // idx=[a={1}, b={0,6}, c={5}]
+    //  i=0 ('a'): prev=1
+    //  i=1 ('b'): prev=6
+    //  i=2 ('c'): prev=? (return false)
+    public boolean isSubsequenceOptimized(String s, String t) {
+        List<Integer>[] idx = new List[256]; // Just for clarity
+        for (int i = 0; i < t.length(); i++) {
+            if (idx[t.charAt(i)] == null)
+                idx[t.charAt(i)] = new ArrayList<>();
+            idx[t.charAt(i)].add(i);
+        }
+        
+        int prev = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (idx[s.charAt(i)] == null) return false; // Note: char of S does NOT exist in T causing NPE
+            int j = Collections.binarySearch(idx[s.charAt(i)], prev);
+            if (j < 0) j = -j - 1;
+            if (j == idx[s.charAt(i)].size()) return false;
+            prev = idx[s.charAt(i)].get(j) + 1;
+        }
+        return true;
+    }
+    
+    /**
      * Write a function to find the longest common prefix string amongst an array of strings.
 
 		If there is no common prefix, return an empty string "".
