bd7dc8f1ae234de091d85f5ad99774734fd0b159
gouthr <gouthr@gmail.com>
2019-02-26 01:07:30 -0800

    Place flowers in nonadjacent locations

--- src/numbers/NumberImpl.java
+++ src/numbers/NumberImpl.java
@@ -2286,5 +2286,44 @@ public class NumberImpl {
         return false;
     }
     
+    /**
+	 * Suppose you have a long flowerbed in which some of the plots are planted
+	 * and some are not. However, flowers cannot be planted in adjacent plots -
+	 * they would compete for water and both would die.
+	 * 
+	 * Given a flowerbed (represented as an array containing 0 and 1, where 0
+	 * means empty and 1 means not empty), and a number n, return if n new
+	 * flowers can be planted in it without violating the no-adjacent-flowers
+	 * rule.
+	 * 
+	 * Example 1: Input: flowerbed = [1,0,0,0,1], n = 1 Output: True Example 2:
+	 * Input: flowerbed = [1,0,0,0,1], n = 2 Output: False
+	 * 
+	 * @param flowerbed
+	 * @param n
+	 * @return
+	 */
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (flowerbed == null || flowerbed.length == 0) {
+            return false;
+        }
+        if (n == 0) {
+            return true;
+        }
+        
+        int cnt = 0;
+        for (int i=0; i<flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                int prev = i>0?flowerbed[i-1]:0;
+                int next = i<(flowerbed.length-1)?flowerbed[i+1]:0;
+                if (prev == 0 && next == 0) {
+                    flowerbed[i] = 1;
+                    cnt++;
+                }
+            }
+        }
+        return cnt >= n;
+    }
+    
     public NumberImpl(){}
 }
