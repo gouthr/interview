5fb49d42a56aa6ba8e3987a590e320f09540accc
gouthr <gouthr@gmail.com>
2019-02-27 11:22:02 -0800

    paint n houses using k colors - find min cost

--- src/numbers/NumberImpl.java
+++ src/numbers/NumberImpl.java
@@ -2055,6 +2055,52 @@ public class NumberImpl {
     }
     
     /**
+	 * Generic version of the above problem. There are a row of n houses, each
+	 * house can be painted with one of the k colors. The cost of painting each
+	 * house with a certain color is different. You have to paint all the houses
+	 * such that no two adjacent houses have the same color.
+	 * 
+	 * The cost of painting each house with a certain color is represented by a
+	 * n x k cost matrix. For example, costs[0][0] is the cost of painting house
+	 * 0 with color 0; costs[1][2] is the cost of painting house 1 with color 2,
+	 * and so on... Find the minimum cost to paint all houses.
+	 * 
+	 * @param costs
+	 * @return
+	 */
+    public int minCostII(int[][] costs) {
+        if (costs == null || costs.length == 0) {
+            return 0;
+        }
+        
+        for (int i=1; i<costs.length; i++) {
+            for (int j=0; j<costs[0].length; j++) {
+                costs[i][j] += findMin(costs, i-1, j);
+            }
+        }
+        
+        int min = Integer.MAX_VALUE;
+        for(int i=0; i<costs[0].length; i++) {
+            if (min > costs[costs.length-1][i]) {
+                min = costs[costs.length-1][i];
+            }
+        }
+        return min;
+    }
+    
+    private int findMin(int[][] costs, int i, int j) {
+        int min = Integer.MAX_VALUE;
+        for (int k=0; k<costs[0].length; k++) {
+            if (k != j) {
+                if (min > costs[i][k]) {
+                    min = costs[i][k];
+                }
+            }
+        }
+        return min;
+    }
+    
+    /**
      * We are given an array asteroids of integers representing asteroids in a row.
 
 		For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
