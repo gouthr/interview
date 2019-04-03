ed6f2328c2bbbc2cb9fb919b9b31c1f71ee49f67
gouthr <gouthr@gmail.com>
2019-04-03 00:32:52 -0700

    sum of left leaves

--- src/trees/TreeImpl.java
+++ src/trees/TreeImpl.java
@@ -1273,6 +1273,38 @@ public class TreeImpl {
         }
         return root;
     }
+    
+    /**
+     * Find the sum of all left leaves in a given binary tree.
+
+		Example:
+		
+		    3
+		   / \
+		  9  20
+		    /  \
+		   15   7
+		
+		There are two left leaves in the binary tree, with values 9 and 15 respectively. Return 24.
+     * @param root
+     * @return
+     */
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int res = 0;
+        if (root.left != null) {
+            if (root.left.left == null && root.left.right == null) {
+                res += root.left.data;
+            } else {
+                res += sumOfLeftLeaves(root.left);
+            }
+        }
+        res += sumOfLeftLeaves(root.right);
+        
+        return res;
+    }
 	
 	/**
 	 * Tree node with parent pointer.
