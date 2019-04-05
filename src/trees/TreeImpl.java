ab70a65baab1aae0a9cf92933db0b303651cb664
gouthr <gouthr@gmail.com>
2019-04-05 00:43:01 -0700

    Sum all paths in a binary tree

--- src/trees/TreeImpl.java
+++ src/trees/TreeImpl.java
@@ -1305,6 +1305,47 @@ public class TreeImpl {
         
         return res;
     }
+    
+    /**
+     * Given a binary tree containing digits from 0-9 only, each root-to-leaf path could represent a number.
+
+		An example is the root-to-leaf path 1->2->3 which represents the number 123.
+		
+		Find the total sum of all root-to-leaf numbers.
+		
+		Note: A leaf is a node with no children.
+		
+		Example:
+		
+		Input: [1,2,3]
+		    1
+		   / \
+		  2   3
+		Output: 25
+		Explanation:
+		The root-to-leaf path 1->2 represents the number 12.
+		The root-to-leaf path 1->3 represents the number 13.
+		Therefore, sum = 12 + 13 = 25.
+     */
+    private int totalSum = 0;
+    public int sumNumbers(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        sumNumbersUtil(root, 0);
+        return totalSum;
+    }
+    
+    private void sumNumbersUtil(TreeNode root, int partialSum) {
+        if (root != null) {
+            if (root.left == null && root.right == null) {
+                totalSum += (partialSum*10) + root.data;
+                return;
+            }
+            sumNumbersUtil(root.left, partialSum*10 + root.data);
+            sumNumbersUtil(root.right, partialSum*10 + root.data);
+        }
+    }
 	
 	/**
 	 * Tree node with parent pointer.
