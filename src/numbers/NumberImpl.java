facd975e40d83bb5d7c32d43666297583de4254d
gouthr <gouthr@gmail.com>
2020-07-09 14:29:12 -0700

    min triangle

--- src/numbers/NumberImpl.java
+++ src/numbers/NumberImpl.java
@@ -2128,6 +2128,37 @@ public class NumberImpl {
     }
     
     /**
+     * Given a triangle, find the minimum path sum from top to bottom. 
+     * Each step you may move to adjacent numbers on the row below.
+
+		For example, given the following triangle
+		
+		[
+		     [2],
+		    [3,4],
+		   [6,5,7],
+		  [4,1,8,3]
+		]
+		The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+     * @param triangle
+     * @return
+     */
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] res = new int[triangle.get(n-1).size()];
+        for (int i=0; i<res.length; i++) {
+            res[i] = triangle.get(n-1).get(i);
+        }
+        for (int i=n-2; i>=0; i--) {
+            List<Integer> list = triangle.get(i);
+            for (int j=0; j<list.size(); j++) {
+                res[j] = list.get(j) + Math.min(res[j], res[j+1]);
+            }
+        }
+        return res[0];
+    }
+    
+    /**
      * We are given an array asteroids of integers representing asteroids in a row.
 
 		For each asteroid, the absolute value represents its size, and the sign represents its direction (positive meaning right, negative meaning left). Each asteroid moves at the same speed.
