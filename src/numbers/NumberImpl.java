df016a5762c7684e487a5f3c4f1b90ab3e4926c3
gouthr <gouthr@gmail.com>
2020-08-02 16:25:33 -0700

    subArray sum equals k

--- src/numbers/NumberImpl.java
+++ src/numbers/NumberImpl.java
@@ -308,6 +308,34 @@ public class NumberImpl {
 	}
 	
 	/**
+	 * Given an array of integers and an integer k, you need to find the total number of continuous 
+	 * subarrays whose sum equals to k.
+
+		Example 1:
+		
+		Input:nums = [1,1,1], k = 2
+		Output: 2
+	 * @param nums
+	 * @param k
+	 * @return
+	 */
+    public int subarraySum(int[] nums, int k) {
+        int sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        int res = 0;
+
+        for(int i=0; i<nums.length; i++) {
+            sum+=nums[i];
+            if (map.containsKey(sum-k)) {
+                res+=map.get(sum-k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0)+1);
+        }
+        return res;
+    }
+	
+	/**
 	 * Two sum optimized on add method.
 	 */
     private Map<Integer, Integer> numFreq = new HashMap<>();
