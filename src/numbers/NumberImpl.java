3c58a33721bf99d07fe922fc0a44e6b0a77f4bfd
gouthr <gouthr@gmail.com>
2020-08-08 15:11:02 -0700

    product except self optimized

--- src/numbers/NumberImpl.java
+++ src/numbers/NumberImpl.java
@@ -274,6 +274,31 @@ public class NumberImpl {
 		return prod;
 	}
 	
+	/**
+	 * Product except self without additional space.
+	 * 
+	 * @param nums
+	 * @return
+	 */
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+        int left = 1;
+        int right = 1;
+        res[0] = 1;
+    
+        for (int i=1; i<nums.length; i++) {
+            left *= nums[i-1];
+            res[i] = left;
+        }
+        
+        for (int i=nums.length-2; i>=0; i--) {
+            right *= nums[i+1];
+            res[i] *= right;
+        }
+        
+        return res;
+    }
+	
 	/*
 	 * Check if 2 nos. in an array sum up to the given key in less than O(n2)
 	 */
