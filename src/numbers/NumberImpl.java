9995ed78a7b25dc39b264ecd032b127a2184fabd
gouthr <gouthr@gmail.com>
2020-08-22 13:50:28 -0700

    max single digit swap in a number to yield a bigger number

--- src/numbers/NumberImpl.java
+++ src/numbers/NumberImpl.java
@@ -2830,5 +2830,42 @@ public class NumberImpl {
         return false;
     }
     
+    /**
+     * Given a non-negative integer, you could swap two digits at most once to get the maximum valued number. 
+     * Return the maximum valued number you could get.
+
+		Example 1:
+		Input: 2736
+		Output: 7236
+		Explanation: Swap the number 2 and the number 7.
+		Example 2:
+		Input: 9973
+		Output: 9973
+		Explanation: No swap.
+     * @param num
+     * @return
+     */
+    public int maximumSwap(int num) {
+        char[] digits = Integer.toString(num).toCharArray();
+        int[] buckets = new int[10];
+        
+        for (int i=0; i<digits.length; i++) {
+            buckets[digits[i]-'0'] = i;
+        }
+        
+        for (int i=0; i<digits.length; i++) {
+            for (int k=9;  k>digits[i]-'0'; k--) {
+                if (buckets[k] > i) {
+                    char tmp = digits[i];
+                    digits[i] = digits[buckets[k]];
+                    digits[buckets[k]] = tmp;
+                    return Integer.parseInt(String.valueOf(digits));
+                }
+            }
+        }
+        
+        return num;
+    }
+    
     public NumberImpl(){}
 }
