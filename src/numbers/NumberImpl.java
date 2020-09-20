328b5121fc059df4e1662d419a9492639ada8b27
gouthr <gouthr@gmail.com>
2020-09-19 17:32:19 -0700

    union-find merge email accounts

--- src/numbers/NumberImpl.java
+++ src/numbers/NumberImpl.java
@@ -14,6 +14,7 @@ import java.util.Queue;
 import java.util.Random;
 import java.util.Set;
 import java.util.Stack;
+import java.util.TreeSet;
 
 public class NumberImpl {
 
@@ -2865,6 +2866,91 @@ public class NumberImpl {
         }
         
         return num;
+    }
+    
+    /**
+     * Union-find algorithm.
+     * Given a list accounts, each element accounts[i] is a list of strings, where the first element accounts[i][0] 
+     * is a name, and the rest of the elements are emails representing emails of the account.
+
+       Now, we would like to merge these accounts. Two accounts definitely belong to the same person if there is some 
+       email that is common to both accounts. Note that even if two accounts have the same name, they may belong to 
+       different people as people could have the same name. A person can have any number of accounts initially, 
+       but all of their accounts definitely have the same name.
+
+      After merging the accounts, return the accounts in the following format: the first element of each account is the 
+      name, and the rest of the elements are emails in sorted order. The accounts themselves can be returned in any 
+      order.
+
+	Example 1:
+	Input: 
+	accounts = [["John", "johnsmith@mail.com", "john00@mail.com"], ["John", "johnnybravo@mail.com"], 
+	["John", "johnsmith@mail.com", "john_newyork@mail.com"], ["Mary", "mary@mail.com"]]
+	Output: [["John", 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com'], 
+	["John", "johnnybravo@mail.com"], ["Mary", "mary@mail.com"]]
+	Explanation: 
+	The first and third John's are the same person as they have the common email "johnsmith@mail.com".
+	The second John and Mary are different people as none of their email addresses are used by other accounts.
+	We could return these lists in any order, for example the answer [['Mary', 'mary@mail.com'], 
+	['John', 'johnnybravo@mail.com'], 
+	['John', 'john00@mail.com', 'john_newyork@mail.com', 'johnsmith@mail.com']] would still be accepted.
+     * @param accounts
+     * @return
+     */
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        if (accounts == null || accounts.size() == 0) {
+            return new ArrayList<List<String>>();
+        }
+        
+        int[] parents = new int[accounts.size()];
+        
+        for (int i=0; i<accounts.size(); i++) {
+            parents[i] = i;    
+        }
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (int i=0; i<accounts.size(); i++) {
+            for (int j=1; j<accounts.get(i).size(); j++) {
+                String email = accounts.get(i).get(j);
+                Integer index = map.get(email);
+                if (index == null) {
+                    index = i;
+                    parents[index] = i;
+                } else {
+                    int p1 = find(parents, i);
+                    int p2 = find(parents, index);
+                    parents[p2] = p1;
+                }
+                map.put(email, index);
+            }
+        }
+        
+        Map<Integer, Set<String>> owners = new HashMap<>();
+        for (int i=0; i<accounts.size(); i++) {
+            int parent = find(parents, i);
+            List<String> emails = accounts.get(i);
+            owners.putIfAbsent(parent, new TreeSet<String>());
+            owners.get(parent).addAll(emails.subList(1, emails.size()));
+        }
+        
+        List<List<String>> res = new ArrayList<>();
+        for (Integer i : owners.keySet()) {
+            String name = accounts.get(i).get(0);
+            List<String> tmp = new ArrayList<>();
+            tmp.add(name);
+            tmp.addAll(owners.get(i));
+            res.add(tmp);
+        }
+        
+        return res;
+    }
+    
+    private int find(int[] parents, int index) {
+        if (parents[index] == index) {
+            return index;
+        }
+        return find(parents, parents[index]);
     }
     
     public NumberImpl(){}
