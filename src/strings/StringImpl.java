b52e6115b7b0854fd957e583f664f06b48494cfd
gouthr <gouthr@gmail.com>
2020-11-17 13:29:07 -0800

    Partition string into substrings - partition labels

--- src/strings/StringImpl.java
+++ src/strings/StringImpl.java
@@ -1948,6 +1948,48 @@ public class StringImpl {
     }
     
     /**
+     * A string S of lowercase English letters is given. We want to partition this string into 
+     * as many parts as possible so that each letter appears in at most one part, 
+     * and return a list of integers representing the size of these parts.
+
+		Example 1:
+		
+		Input: S = "ababcbacadefegdehijhklij"
+		Output: [9,7,8]
+		Explanation:
+		The partition is "ababcbaca", "defegde", "hijhklij".
+		This is a partition so that each letter appears in at most one part.
+		A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits S into less parts.
+     * @param S
+     * @return
+     */
+    public List<Integer> partitionLabels(String S) {
+        List<Integer> res = new ArrayList<>();
+        
+        if (S == null || S.length() == 0) {
+            return res;
+        }
+        
+        int[] map = new int[26];
+        
+        for (int i=0; i<S.length(); i++) {
+            // index of the last occurence of the alphabet
+            map[S.charAt(i) - 'a'] = i;
+        }
+        
+        int last = 0;
+        int start = 0;
+        for (int i=0; i<S.length(); i++) {
+            last = Math.max(last, map[S.charAt(i) - 'a']);
+            if (last == i) {
+                res.add(S.substring(start, last+1).length());
+                start = last + 1;
+            }
+        }
+        return res;   
+    }
+    
+    /**
 	 * There is a new alien language which uses the latin alphabet. However, the
 	 * order among letters are unknown to you. You receive a list of non-empty
 	 * words from the dictionary, where words are sorted lexicographically by
