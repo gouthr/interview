4aea009bf5081fae37ec093608618fd6950ccece
gouthr <gouthr@gmail.com>
2021-07-06 11:56:30 -0700

    Update NumberImpl.java

--- src/numbers/NumberImpl.java
+++ src/numbers/NumberImpl.java
@@ -3122,6 +3122,42 @@ public class NumberImpl {
         } 
         return res;
     }
+	
+    /**
+    Today, the bookstore owner has a store open for customers.length minutes.  Every minute, some number of customers (customers[i]) enter the store, and all those customers leave after the end of that minute.
+
+	On some minutes, the bookstore owner is grumpy.  If the bookstore owner is grumpy on the i-th minute, grumpy[i] = 1, otherwise grumpy[i] = 0.  When the bookstore owner is grumpy, the customers of that minute are not satisfied, otherwise they are satisfied.
+
+	The bookstore owner knows a secret technique to keep themselves not grumpy for minutes minutes straight, but can only use it once.
+
+	Return the maximum number of customers that can be satisfied throughout the day.
+
+
+
+	Example 1:
+
+	Input: customers = [1,0,1,2,1,1,7,5], grumpy = [0,1,0,1,0,1,0,1], minutes = 3
+	Output: 16
+	Explanation: The bookstore owner keeps themselves not grumpy for the last 3 minutes. 
+	The maximum number of customers that can be satisfied = 1 + 1 + 1 + 1 + 7 + 5 = 16.
+    */
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int n = customers.length;
+        int sum = 0;
+        int makeSatisfied = 0;
+        int makeSatisfiedMax = 0;
+        for(int i=0; i<n; i++) {
+            if (grumpy[i] == 0) {
+                sum+=customers[i];
+            }
+            makeSatisfied+=customers[i] * grumpy[i];
+            if (i >= minutes) {
+                makeSatisfied -= customers[i-minutes] * grumpy[i-minutes];
+            }
+            makeSatisfiedMax = Math.max(makeSatisfiedMax, makeSatisfied);
+        }
+        return sum + makeSatisfiedMax;
+    }
     
     public NumberImpl(){}
 }
